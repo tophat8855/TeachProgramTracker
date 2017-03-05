48445eba6021c3b0fa264a8fd980f424f163182f
Pavlo Manovi <pavlo.manovi@gmail.com>
2017-03-04 19:01:08 -0800

    Working form to database path.

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -2,4 +2,17 @@ class ProceduresController < ApplicationController
   def index
   	@procedures = Procedure.all
   end
+
+  def create
+  	Procedure.create(procedure_params)
+  	redirect_to procedures_path
+  end
+
+  def new
+  	@procedure = Procedure.new
+  end
+
+  def procedure_params
+  	params.require(:procedure).permit(:name, :date)
+  end
 end
