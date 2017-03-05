2c094709b19d18dd8e17122f2e05e889a0435b03
tophat8855 <hrfarley@gmail.com>
2017-03-04 17:30:01 -0800

    Create locations table, add location_id to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304230203) do
+ActiveRecord::Schema.define(version: 20170305012330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -30,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170304230203) do
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
     t.boolean  "trainer",                default: false
+    t.integer  "location_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
