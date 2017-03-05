4e8cda0e5bf1ad0a342007702cd7245264e19e31
Elliott Etzkorn <elliottetzkorn@gmail.com>
2017-03-04 16:26:42 -0800

    Devise setup part 2 (#3)
    * adds admin level users
    
    * adds trainer level users
    
    * adds placeholder views for dashboards

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :trainers
+  devise_for :admins
   devise_for :users
+  resources :admin, only: [:index]
+  resources :trainer, only: [:index]
   root 'home#index'
 end
