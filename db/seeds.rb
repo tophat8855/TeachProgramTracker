2dc4d3bd6166166131e29e6c45927a1ad22e9e7a
tophat8855 <hrfarley@gmail.com>
2017-03-04 20:01:40 -0800

    seeds file for ease of creating dummy users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+locations = Location.create([{name: 'San Francisco'}, {name: 'Contra Costa'}])
+
+User.create({email: 'test@gmail.com', password: 'password', password_confirmation: 'password', name: 'Test1', admin: true, trainer: true, location_id: locations.first.id})
+User.create({email: 'test2@gmail.com', password: 'password', password_confirmation: 'password', name: 'Test2' status: 'R2'})
