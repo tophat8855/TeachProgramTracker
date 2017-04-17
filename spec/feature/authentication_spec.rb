4017f00ffa0a379a3774fed3007d3d5e563d80d9
tophat8855 <hrfarley@gmail.com>
2017-04-16 19:03:04 -0700

    Add factories for easy testing

--- spec/feature/authentication_spec.rb
+++ spec/feature/authentication_spec.rb
@@ -1,14 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'authentication', type: :feature do
-  before do
-    u = User.create(email: 'user@example.com',
-            password: 'password',
-            password_confirmation: 'password',
-            name: 'Username',
-            residency_location_id: 1)
-  end
-
   context 'when the user is not authenticated' do
     it 'does not log the user in' do
       visit '/'
@@ -22,11 +14,18 @@ RSpec.describe 'authentication', type: :feature do
   end
 
   context 'when the user is authenticated' do
+    let(:email) { 'user@example.com' }
+    let(:password) { 'password' }
+
+    before do
+      FactoryGirl.create(:user, email: email, password: password, password_confirmation: password)
+    end
+
     it 'logs the user in' do
       visit '/'
 
-      fill_in 'user_email', with: 'user@example.com'
-      fill_in 'user_password', with: 'password'
+      fill_in 'user_email', with: email
+      fill_in 'user_password', with: password
 
       click_button 'Log in'
       expect(page).to have_content 'Signed in successfully.'
