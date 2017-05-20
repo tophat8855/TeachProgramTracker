205dafa17a06a2a0e4de31d3d1218272a5245eaf
tophat8855 <hrfarley@gmail.com>
2017-05-20 08:37:31 -0700

    Revert routes.rb to previous state

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path_prefix: 'my', controllers: {
-          sessions: 'users/sessions',
-          registrations: 'users/registrations'
-        }
+  devise_for :users, path_prefix: 'my'
   post 'users/invite', to: 'users#invite'
 
   root 'home#index'
