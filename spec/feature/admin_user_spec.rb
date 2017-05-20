7ba9a58cbabdf8a15daab8f0495e46f3ab3cbad9
tophat8855 <hrfarley@gmail.com>
2017-05-20 11:53:03 -0700

    User table displays residency/trainer status

--- spec/feature/admin_user_spec.rb
+++ spec/feature/admin_user_spec.rb
@@ -11,11 +11,20 @@ RSpec.describe 'Admin management of users', type: :feature do
     password_confirmation: 'password')
   }
 
+  let!(:trainer) { FactoryGirl.create(:user,
+    name: 'Trainer Name',
+    email: 'trainer@email.com',
+    residency_location_id: location.id,
+    trainer: true,
+    password: 'password',
+    password_confirmation: 'password')
+  }
+
   before do
     login_admin
   end
 
-  fdescribe 'list of users' do
+  describe 'list of users' do
     it 'shows all users and their corresponding data' do
       click_on 'Users'
 
@@ -23,7 +32,12 @@ RSpec.describe 'Admin management of users', type: :feature do
       expect(page).to have_content 'Resident Name'
       expect(page).to have_content 'resident@email.com'
       expect(page).to have_content location.name
+      expect(page).to have_content 'R2'
 
+      expect(page).to have_content 'Trainer Name'
+      expect(page).to have_content 'trainer@email.com'
+      expect(page).to have_content location.name
+      expect(page).to have_content 'Trainer'
     end
   end
 
